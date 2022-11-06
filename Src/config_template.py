9e3f9a380c1a3015aab8aec1da9559a64443ddc0
Ivan Johnson <git@IvanJohnson.net>
2022-11-06 17:23:40 -0500

    Update migration script

---
+++ Src/config_template.py
@@ -0,0 +1,12 @@
+# The LLS_URL & LLS_TOKEN values are used to connect to and authenticate with
+# the LifeLogServer. The commented values below are provided to lower the
+# barrier to enter the LL ecosystem; feel free to use them to mess around with
+# LL for a bit. This is public a demo account though; don't be surprised if
+# another user deletes data that you add, and the owner reserves the right to
+# terminate the account without notice.
+#
+# LLS_URL='https://lifelog.ivanjohnson.net'
+# LLS_TOKEN='ebf1a0a3-878e-45a5-9850-76ef4f00b5d1'
+
+LLS_URL=None
+LLS_TOKEN=None
--- Src/migrate.py
+++ Src/migrate.py
@@ -9,37 +9,40 @@ import lls
 
 def main():
     config = lls.get_config()
-    url = config['LLS_URL'] + '/api/v1/weight/record'
+    url = config['LLS_URL'] + '/api/v1/weight/entry'
 
     offset = time.mktime((datetime.date(1970, 1,1)).timetuple())
     #data.csv is a csv of format:
-    #YYYY-MM-DD\tweight
+    #YYYY-MM-DD-HH-MM-SS\tweight
     with open('data.csv', newline='') as fd:
         reader = csv.reader(fd, skipinitialspace=True, delimiter='\t', strict=True)
         for row in reader:
+            print(row)
             assert(len(row)==2)
             sdate = row[0]
             weight = float(row[1])
 
             date_components = sdate.split('-')
-            assert(len(date_components) == 3)
-            year = date_components[0]
-            month = date_components[1]
-            day = date_components[2]
+            assert(len(date_components) == 6)
+            year = int(date_components[0])
+            month = int(date_components[1])
+            day = int(date_components[2])
+            hour = int(date_components[3])
+            minute = int(date_components[4])
+            second = int(date_components[5])
 
-            date = datetime.date(year=int(year), month=int(month), day=int(day))
+            date = datetime.datetime(year=year, month=month, day=day, hour=hour, minute=minute, second=second)
             seconds = time.mktime(date.timetuple())
             unix = seconds - offset
 
             cacheid = uuid.uuid4()
 
 
-            query_params = {'datetime': int(unix), 'weight': float(weight)}
+            query_params = {'datetime': int(unix), 'weight': float(weight), 'units': 'kilograms'}
             response = requests.post(url, params=query_params, headers={'token': config['LLS_TOKEN']})
             if response.status_code != 201:
+                import pdb; pdb.set_trace()
                 print(f"{response.status_code}\t{sdate}\t{str(weight)}\t{cacheid}")
 
 assert(__name__ == "__main__")
 main()
-
-#original: acc0300a-e082-45cb-a8ad-00548c59daab|8c94d994-5b3d-483e-8563-266ad0ba8b71|1592104858|83.6
