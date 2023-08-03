11728ae0d0a7670f730e881ac3c2726518b9b060
Ivan Johnson <git@IvanJohnson.net>
2023-08-03 09:04:10 -0400

    Use local development server by default

--- Src/config_template.py
+++ Src/config_template.py
@@ -1,12 +1,2 @@
-# The LLS_URL & LLS_TOKEN values are used to connect to and authenticate with
-# the LifeLogServer. The commented values below are provided to lower the
-# barrier to enter the LL ecosystem; feel free to use them to mess around with
-# LL for a bit. This is public a demo account though; don't be surprised if
-# another user deletes data that you add, and the owner reserves the right to
-# terminate the account without notice.
-#
-# LLS_URL='https://lifelog.ivanjohnson.net'
-# LLS_TOKEN='ebf1a0a3-878e-45a5-9850-76ef4f00b5d1'
-
-LLS_URL=None
-LLS_TOKEN=None
+LLS_URL='127.0.0.1:5000'
+LLS_TOKEN='dev-key'
